2e3f09e03a5881aa99d0a4b895cb7ee2c37e9977
João Vitor Borges Godinho <398762@unoesc.lan>
2024-05-16 21:45:45 -0300

    Aula 16/05/2024

--- afazeresmvc/controllers/editar.php
+++ afazeresmvc/controllers/editar.php
@@ -1,67 +1,55 @@
 <?php
 
-include "config.php";
-include "banco.php";
-include "ajudantes.php";
-include "classes/Tarefa.php";
-include "classes/Anexo.php";
-include "classes/RepositorioTarefas.php";
+$tarefa = $repositorio_tarefas->buscar($_GET['id']);
 
-$repositorio_tarefas = new RepositorioTarefas($mysqli);
-
-$exibir_tabela = true;
-
+$exibir_tabela = false;
 $tem_erros = false;
-$erros_validacao = [];
-
-$tarefa = new Tarefa();
-$tarefa->setPrioridade(1);
+$erros_validacao = array();
 
 if (tem_post()) {
-    if (array_key_exists('nome', $_POST) && strlen($_POST['nome']) > 0) {
+
+    if (isset($_POST['nome']) && strlen($_POST['nome']) > 0) {
         $tarefa->setNome($_POST['nome']);
     } else {
         $tem_erros = true;
         $erros_validacao['nome'] = 'O nome da tarefa é obrigatório!';
     }
 
-    if (array_key_exists('descricao', $_POST)) {
+    if (isset($_POST['descricao'])) {
         $tarefa->setDescricao($_POST['descricao']);
     } else {
-        $tarefa['descricao'] = '';
+        $tarefa->setDescricao('');
     }
 
-    if (array_key_exists('prazo', $_POST) && strlen($_POST['prazo']) > 0) {
+    if (isset($_POST['prazo']) && strlen($_POST['prazo']) > 0) {
         if (validar_data($_POST['prazo'])) {
             $tarefa->setPrazo(traduz_data_br_para_objeto($_POST['prazo']));
         } else {
             $tem_erros = true;
             $erros_validacao['prazo'] = 'O prazo não é uma data válida!';
         }
+    } else {
+        $tarefa->setPrazo('');
     }
 
     $tarefa->setPrioridade($_POST['prioridade']);
 
-    if (array_key_exists('concluida', $_POST)) {
+    if (isset($_POST['concluida'])) {
         $tarefa->setConcluida(true);
     } else {
         $tarefa->setConcluida(false);
     }
 
     if (! $tem_erros) {
-        $repositorio_tarefas->salvar($tarefa);
+        $repositorio_tarefas->atualizar($tarefa);
 
         if (isset($_POST['lembrete']) && $_POST['lembrete'] == '1') {
             enviar_email($tarefa);
         }
 
-        header('Location: tarefas.php');
+        header('Location: index.php?rota=tarefas');
         die();
     }
 }
 
-$tarefas = $repositorio_tarefas->buscar();
-
-include "template.php";
-
-?>
+require __DIR__ . "/../views/template.php";
